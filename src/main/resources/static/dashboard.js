f846a5bf1390d9d2599ea77d9f08086e0e4a54bd
Ricardo Rios Donato <rrios62@gmail.com>
2023-07-18 10:50:24 -0600

    Correcciones a grafica

--- src/main/resources/static/dashboard.js
+++ src/main/resources/static/dashboard.js
@@ -1,34 +1,36 @@
-/* globals Chart:false, feather:false */
-
 (function () {
-  'use strict'
+  'use strict';
 
-  feather.replace({ 'aria-hidden': 'true' })
+  feather.replace({ 'aria-hidden': 'true' });
 
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
+  // Obtener los datos de la tabla
+  var tabla = document.querySelector('.table.table-striped.table-sm');
+  var filas = tabla.querySelectorAll('tbody tr');
+
+  // Crear arreglos para almacenar las fechas y los datos de facturas por día
+  var fechas = [];
+  var facturasPorDia = [];
+
+  // Obtener las fechas y los datos de facturas por día de la tabla
+  for (var i = 0; i < filas.length; i++) {
+    var fechaCell = filas[i].querySelector('.fecha');
+    var facturaCell = filas[i].querySelector('td:nth-child(3)');
+
+    var fecha = fechaCell.innerText;
+    var factura = parseInt(facturaCell.innerText);
+
+    fechas.push(fecha);
+    facturasPorDia.push(factura);
+  }
+
+  // Crear la gráfica con los datos obtenidos de la tabla
+  var ctx = document.getElementById('myChart');
   var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: [
-        'Lunes',
-        'Martes',
-        'Miércoles',
-        'Jueves',
-        'Viernes',
-        'Sábado'
-      ],
+      labels: fechas,
       datasets: [{
-        data: [
-          15,
-          21,
-          18,
-          24,
-          23,
-          24,
-          12
-        ],
+        data: facturasPorDia,
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
@@ -48,5 +50,5 @@
         display: false
       }
     }
-  })
-})()
+  });
+})();
