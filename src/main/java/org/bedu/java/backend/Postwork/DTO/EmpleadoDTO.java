6f34b9760c126f12c94fc5eb54d60146cf2f31fa
rrios62 <118863866+rrios62@users.noreply.github.com>
2023-07-21 13:09:20 -0600

    Se anexa DTO para obtener lista de empleados

---
+++ src/main/java/org/bedu/java/backend/Postwork/DTO/EmpleadoDTO.java
@@ -0,0 +1,16 @@
+package org.bedu.java.backend.Postwork.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmpleadoDTO {
+    private Long id;
+    private String nombreEmpleado;
+
+}
